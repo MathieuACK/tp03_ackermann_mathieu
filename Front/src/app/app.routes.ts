be4f787df13f2e3b565bc1abd8d58aa94419d7b4
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2025-10-20 19:40:39 +0200

    feat: add HTTP client provider and define application routes for pollution components

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PollutionReportComponent } from './components/pollution-report/pollution-report.component';
+import { PollutionListComponent } from './components/pollution-list/pollution-list.component';
+import { PollutionDetailComponent } from './components/pollution-detail/pollution-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: PollutionListComponent },
+  { path: 'report', component: PollutionReportComponent },
+  { path: 'pollution/:id', component: PollutionDetailComponent },
+  { path: '**', redirectTo: '/list' },
+];
